0e2a6e40d0abb5dff471c1149c6b96812186ecdf
felipersan <feliperodrigues.san@gmail.com>
2022-11-29 15:38:37 -0300

    att - /privacidade - crate privacy page

--- src/pages/privacidade/index.tsx
+++ src/pages/privacidade/index.tsx
@@ -19,91 +19,46 @@ export default function Privacidade() {
           </div>
 
           <div className="politica container">
-            <div style={{ marginBottom: '2rem' }} className="politica__sectionTitle">
-              <h2 className="politica__title">Esta Política de Privacidade tem como objetivo informar você (doravante “Usuário”), que acessa a página da Facinnius na Internet, quais informações são coletadas, como elas são tratadas, bem como as formas de compartilhamento e de exclusão desses dados. </h2>
-              <h2 className="politica__title">A presente Política de Privacidade é aplicável ao presente site da Facinnius Empresa Ltda. <br /> Com a aceitação desta Política de Privacidade pelo Usuário, este concorda e permite o acesso as suas informações a partir do seu primeiro acesso ao site. Caso reste alguma dúvida, após a leitura desta Política de Privacidade, entre em contato conosco, entre em contato.</h2>
-            </div>
-
             <div dangerouslySetInnerHTML={{__html: `<div id="wa-comptext-kdp3kgp98xm6w0" >
-						<p><span><strong><span >1) Quais informações coletamos:</span></strong></span></p>            
-						<p><span >Para os fins desta Política de Privacidade, aplicam-se as seguintes definições:</span></p>
+				<p><span ><strong>1.1 </strong> Tecnologias como cookies, pixel tags, identificadores de dispositivo ou outros e armazenamento local ("Cookies e tecnologias semelhantes") são usadas durante a sua navegação no site para transmitir, proteger e entender Serviços e anúncios, dentro e fora dos Serviços.</span></p>
             <br>
-						<p><span >Nós coletamos todas as informações inseridas pelo Usuário quando de sua utilização do Site, como, por exemplo, no preenchimento dos formulários que disponibilizamos no Site, bem como na realização de cadastros ou eventuais comentários. Em síntese, são todas as informações ativamente disponibilizadas pelo Usuário na utilização do Site.</span></p>
-						<p><span> </span></p>
+				<p><span><strong>1.1.1 </strong> Cookies são pequenos arquivos armazenados no navegador do Usuário, celular ou outro dispositivo. Pixel tags (também conhecidos como GIFs limpos, web beacons ou pixels) são pequenos blocos de código em uma página da web que permitem que elas realizem ações como ler e armazenar cookies e transmitir informações para os parceiros. A conexão resultante pode incluir informações como o endereço de IP de um dispositivo, a hora em que uma pessoa visualizou o pixel, um identificador associado ao navegador ou dispositivo e o tipo de navegador em uso.</span></p>
+			<br/>	
+				<p><span><strong>1.1.2 </strong> Armazenamento local é uma tecnologia padrão da indústria que permite que um site ou aplicativo armazene e recupere dados do computador, celular ou outro dispositivo de uma pessoa. Alguns exemplos incluem dispositivos ou armazenamento local HTML5 e caching.</span></p>
+						
             <br>
-						<p><span >Nós também coletamos informações automaticamente quando o Usuário acessa o Site, tais como características do dispositivo, do navegador, Protocolo de Internet (IP, com data e hora), origem do IP, informações sobre cliques, páginas acessadas, as páginas seguintes acessadas após a saída do Site, ou qualquer termo de busca digitado no Site ou em referência a esse, dentre outros. Também podemos utilizar outras tecnologias, como cookies, pixel tags, beacons e local shared objects, para coletar informações do Usuário e melhorar sua experiência de navegação.</span></p>
-						<p><span> </span></p>
+				<p><span><strong>1.2 </strong>Essas tecnologias poderão ser usadas, entre outros, para permitir que nos e nossos parceiros apresentem os conteúdos e ofertas mais relevantes para você, aprimorar os Serviços e ajudar a manter estes Serviços seguros.</span></p>
+						
             <br>
-						<p><span >É facultado ao Usuário bloquear a coleta de alguns tipos de informações, tais como cookies. Caso essa configuração seja implementada, no entanto, é possível que algumas ferramentas ou tecnologias oferecidas pelo Site não funcionem corretamente.</span></p>
-						<p><span> </span></p>
-            <br>
-						<p><span >Assim, o Usuário desde já se encontra ciente acerca das informações coletadas pelo Site e expressa consentimento livre, expresso e informado com relação à coleta de tais informações.</span></p>
-						<p><span > </span></p>
-            <br>
-						<p><span><strong><span >2. Como utilizamos as informações coletadas </span></strong></span></p>
-						<p><span >As informações coletadas por meio do Site serão utilizadas para:</span></p>
-            <br>
-            
-						<p style="padding-left: 30px;"><span >1. O fornecimento de produtos ou serviços solicitados no Site.</span></p>
-						<p style="padding-left: 30px;"><span >2. Facilitar a identificação do perfil e necessidades dos Usuários, a fim de aprimorar a oferta de produtos e/ou serviços.</span></p>
-						<p style="padding-left: 30px;"><span >3. Enviar informativos sobre produtos e/ou serviços de interesse aos usuários.</span></p>
-						<p style="padding-left: 30px;"><span >4. Divulgar alterações, inovações ou promoções sobre os produtos e serviços.</span></p>
-						<p style="padding-left: 30px;"><span >5. Ofertar produtos e serviços da nossa empresa.</span></p>
-						<p style="padding-left: 30px;"><span> </span></p>
-            <br>
-						<p><span >Utilizamos em nosso Site recursos de campanhas de publicidade baseadas em interesses, listas de remarketing, informações demográficas e de local de destino. As campanhas de publicidade não associam identificações individuais às listas de remarketing, cookies, dados de feeds ou outros identificadores anônimos. Da mesma forma, os recursos aplicados em campanhas não são associados a informações coletadas na página de nosso site e anúncios. Ainda, as informações de identificação individual não são compartilhadas com nenhum Site ou parceiro por meio da tag de remarketing ou qualquer feed de dados que possam estar associados aos nossos anúncios.</span></p>
-						<p><span >O Usuário dá o consentimento livre, expresso e informado para que utilizemos as informações coletadas por meio do Site para fins publicitários, dentro do próprio Site e por meio das informações de contato fornecidas pelo Usuário no Site, nos termos descritos nesta Política de Privacidade.</span></p>
-						<p><span><span >Caso não deseje mais receber informativos publicitários, envie um e-mail para nós, </span><strong style="font-family: 'Open Sans', Arial, Helvetica, 'Liberation Sans', FreeSans, sans-serif;"><a style="color: #3d3d3d;" title="" href="contato"  onclick="return true;" >entre em contato</a></strong><span >.</span></span></p>
-						<p><span >É, ainda, importante atentar que o Site pode conter links para outras páginas, como redes sociais ou lojas virtuais, entre outras, que possuem Política de Privacidade com previsões diversas das deste documento. Dessa forma, não se responsabiliza pela coleta, utilização, compartilhamento e armazenamento de dados dos Usuários pelos responsáveis por tais páginas.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >3. Com quem compartilhamos as informações coletadas </span></strong></span></p>
-						<p><span >Poderemos compartilhar os dados em referência com as demais empresas de parceria e com terceiros de forma anonimizada, com o que o Usuário desde logo concorda expressamente, o que poderá ocorrer especialmente nas seguintes hipóteses:</span></p>
-						<p style="padding-left: 30px;"><span >1. Caso a viabilização dos negócios e/ou serviços oferecidos dependa do repasse de dados a parceiros.</span></p>
-						<p style="padding-left: 30px;"><span >2. Para proteção dos interesses, em qualquer tipo de conflito, incluindo ações judiciais, como, mas não se limitando a, quando exista a necessidade de identificar ou revelar dados de Usuário que esteja utilizando o Site com propósitos ilícitos.</span></p>
-						<p style="padding-left: 30px;"><span >3. No caso de transações e alterações societárias nos envolvendo.</span></p>
-						<p style="padding-left: 30px;"><span >4. Mediante ordem judicial ou pelo requerimento de autoridades administrativas que detenham competência legal para sua requisição.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >4. Como armazenamos as informações coletadas </span></strong></span></p>
-						<p><span >As informações coletadas são tratadas como sigilosas, e qualquer funcionário que entre em contato com elas se comprometerá a não desvirtuar sua utilização, bem como em não as usar de modo destoante do previsto nesta Política de Privacidade. Nós empregamos todos os esforços razoáveis de mercado para garantir a segurança de seus sistemas na guarda de referidos dados.</span></p>
-						<p><span >Para que tais medidas se tornem viáveis tomamos as seguintes precauções:</span></p>
-						<p style="padding-left: 30px;"><span >1. Utilizamos os métodos padrões e de mercado para anonimizar os dados coletados.</span></p>
-						<p style="padding-left: 30px;"><span >2. Possuímos software de proteção contra acesso não autorizado aos nossos sistemas.</span></p>
-						<p style="padding-left: 30px;"><span >3. Somente autorizamos o acesso de pessoas previamente estabelecidas aos locais onde armazenamos as informações.</span></p>
-						<p style="padding-left: 30px;"><span >4. Aqueles que entrarem em contato com as informações deverão se comprometer a manter sigilo absoluto. A quebra do sigilo acarretará responsabilidade civil e o responsável será processado nos moldes da legislação brasileira.</span></p>
-						<p><span >Esta Política de Privacidade representa nossos esforços no sentido de resguardar as informações dos Usuários que acessam nosso Site. No entanto, em razão da própria natureza da Internet, não é possível garantir que terceiros mal intencionados não logrem sucesso em acessar indevidamente as informações armazenadas por nós.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >5. Quando excluímos as informações </span></strong></span></p>
-						<p><span >As informações referidas na presente Política de Privacidade serão automaticamente excluídas quando deixarem de ser úteis aos propósitos para os quais foram coletadas.</span></p>
-						<p><span >No entanto, caso o Usuário deseje antecipar essa exclusão, poderá requerê-la através de e-mail ou telefone. Em tal circunstância, empreenderemos os melhores esforços para atender a todos os pedidos no menor espaço de tempo possível.</span></p>
-						<p><span >Porém, o Usuário deve estar ciente de que, mesmo em caso de requisição de exclusão, respeitaremos o prazo de armazenamento mínimo de informações determinado pela legislação brasileira.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >6. Responsabilidades do usuário </span></strong></span></p>
-            <br>
-
-						<p><span >O Usuário declara-se ciente, desde já, que é de sua exclusiva responsabilidade:</span></p>
-						<p style="padding-left: 30px;"><span >1. Inserir apenas informações verdadeiras em nosso site.</span></p>
-						<p style="padding-left: 30px;"><span >2. Zelar pelos dados de sua identificação digital individual sempre que acessar a Internet, informando-os apenas em operações em que exista a proteção de dados.</span></p>
-						<p style="padding-left: 30px;"><span >3. Cumprir rigorosamente todas as determinações e procedimentos previstos nesta Política.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >7. Atualização desta Política de Privacidade </span></strong></span></p>
-						<p><span >Reservamo-nos o direito de alterar essa Política de Privacidade sempre que necessário, com o fito de fornecer maior segurança e praticidade ao Usuário. Por isso, é importante que o Usuário leia a Política de Privacidade a cada novo acesso ao Site.</span></p>
-						<p><span > </span></p>
-            <br>
-
-						<p><span><strong><span >8. Legislação e foro competentes </span></strong></span></p>
-						<p><span >Esta Política de Privacidade será regida, interpretada e executada de acordo com as leis da República Federativa do Brasil, independentemente dos conflitos dessas leis com leis de outros estados ou países, sendo competente o Foro do local de residência do Usuário, no Brasil, para dirimir qualquer dúvida decorrente deste instrumento. O Usuário consente, expressamente, com a competência desse juízo, e renuncia, neste ato, à competência de qualquer outro foro, por mais privilegiado que seja ou venha a ser.</span></p>
-					</div>`}}/>
+				<p><span><strong>1.3 </strong>Os nomes específicos dos Cookies e tecnologias semelhantes usados podem variar na medida em que os Serviços são melhorados e atualizados, mas eles geralmente se restringem às categorias de uso abaixo:</span></p>
+			`}}/>
           </div>
+		  <div className="politica containerBorda container">
+            <div dangerouslySetInnerHTML={{__html: `<div id="wa-comptext-kdp3kgp98xm6w0" >
+				<p><span ><strong>Autenticação:</strong> Responsável por auxiliar a manter a segurança dos Serviços, oferecendo suporte ou ativando recursos de segurança e ajudando a detectar atividades que violam os Termos de Uso.</span></p>
+            <br>
+				<p><span ><strong>Segurança e integridade do site: </strong> Responsável por indicar que você está conectado(a), para que seja possível oferecer a você os recursos apropriados, bem como para entender como você usa os Serviços do nosso site.</span></p>
+
+			<br/>	
+				<p><span><strong>Anúncios, ideias e medição:</strong> Responsável por analisar comportamento de navegação e veicular anúncios, torná-los mais relevantes para você e analisar Serviços e o seu uso. Por exemplo, é possível utilizar um cookie para descobrir se alguém a quem um anúncio tenha sido veiculado faz uma compra no site do anunciante ou instala o aplicativo anunciado posteriormente. Da mesma forma, parceiros podem usar cookies ou outras tecnologias semelhantes para determinar se nosso site exibiu um de seus anúncios e qual foi o seu desempenho, ou informar como você interage com ele.</span></p>
+						
+            <br>
+				<p><span><strong>Recursos e serviços do site: </strong>Responsável por ajudar a fornecer produtos e serviços, por exemplo, quando você viu ou interagiu com conteúdo dos Serviços, e fornecer a ele outros plug-ins sociais, outras experiências e conteúdos personalizados ou fazer sugestões.</span></p>
+						
+            <br>
+				<p><span><strong>Desempenho: </strong>Responsável por proporcionar a melhor experiência possível, por exemplo, ajudando a rotear o tráfego entre servidores e perceber com que velocidade os Serviços do nosso site são carregados para diferentes pessoas. Ocasionalmente, poderão ser armazenadas informações no seu navegador ou dispositivo para que os recursos em uso carreguem e respondam rapidamente.</span></p>
+			<br>
+				<p><span><strong>Análises e pesquisas: </strong>O objetivo é entender, aprimorar e pesquisar Serviços, incluindo quando você acessa o nosso site ou outros sites e aplicativos a partir do computador ou dispositivo móvel. Por exemplo, é possível utilizar Cookies ou tecnologias semelhantes (incluindo informações do seu dispositivo) para entender como você está usando plug-ins sociais e melhorá-los, podendo ser compartilhadas informações sobre essa análise com os parceiros da da nossa empresa.</span></p>
+			`}}/>
+          </div>
+		  <div className="politica container">
+            <div dangerouslySetInnerHTML={{__html: `<div id="wa-comptext-kdp3kgp98xm6w0" >
+				<p><span ><strong>1.4</strong> É possível que você gerencie preferências de Cookies e outras tecnologias semelhantes a partir da configuração de seu navegador, recusando ou excluindo alguns deles. No entanto, essa opção poderá comprometer os Serviços do nosso site ou impedir o funcionamento de determinadas funcionalidades que integram os Serviços.</span></p>
+           
+			`}}/>
+          </div>
+		  
+		  
 
         </section>
         <Footer />
