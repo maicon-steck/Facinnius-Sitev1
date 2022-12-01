7a7fa93f35319954509d17a6fa9d97430395cb54
felipersan <feliperodrigues.san@gmail.com>
2022-11-30 23:18:13 -0300

    att header and footer logo image

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,76 +1,87 @@
-import Link from "next/link";
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { RiFacebookFill, RiInstagramFill, RiYoutubeFill } from "react-icons/ri";
-import { IoLogoTiktok } from "react-icons/io5";
-import Image from "next/image";
-import logoFacinnius from "../../../public/images/logoFacinnius.png";
-import i18n from "../../i18n";
-
+import Link from 'next/link'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { RiFacebookFill, RiInstagramFill, RiYoutubeFill } from 'react-icons/ri'
+import { IoLogoTiktok } from 'react-icons/io5'
+import Image from 'next/image'
+import logoFacinnius from '../../../public/images/LOGOS_FACINNIUS_REGISTRADOS.png'
+import i18n from '../../i18n'
 
 export default function Footer() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
+
+  function logoSize() {
+    if (typeof window !== 'undefined') {
+      return window.innerWidth
+    }
+  }
 
   return (
     <footer className="footer section">
       <div className="footer__container container grid">
-      <div>
+        <div>
           <Link href="/" className="nav__logo">
             <a
               style={{
-                display: "flex",
-                alignItems: "center",
-                justifyItems: "center",
-                marginLeft: "-10px",
-                marginBottom: "8px",
+                display: 'flex',
+                alignItems: 'center',
+                justifyItems: 'center',
+                marginLeft: '-10px',
+                marginBottom: '8px'
               }}
             >
-              <Image src={logoFacinnius} alt="Logo" />
-              Facinnius
+              <Image
+                src={logoFacinnius}
+                alt="Logo"
+                height={logoSize() > 500 ? 42 : 30}
+                width={logoSize() > 500 ? 162 : 116}
+              />
             </a>
           </Link>
 
           <p
             className="footer__description"
-            dangerouslySetInnerHTML={{ __html: t("home_footer_descricao") }}
+            dangerouslySetInnerHTML={{ __html: t('home_footer_descricao') }}
           />
         </div>
 
         <div className="footer__content">
-        
           <div>
             <h3 className="footer__title">
-              {t("home_footer_companhia_titulo")}
+              {t('home_footer_companhia_titulo')}
             </h3>
 
             <ul className="footer__links">
               <li>
                 <Link href="/lista-produtos" className="footer__link">
                   <a className="footer__link">
-                    {t("home_footer_companhia_produtos")}
+                    {t('home_footer_companhia_produtos')}
                   </a>
                 </Link>
               </li>
               <li>
-                <Link href={i18n.language === 'en' ? '/en/locais' : '/locais'} className="footer__link">
+                <Link
+                  href={i18n.language === 'en' ? '/en/locais' : '/locais'}
+                  className="footer__link"
+                >
                   <a className="footer__link">
-                    {t("home_footer_companhia_linhas")}
+                    {t('home_footer_companhia_linhas')}
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/contato" className="footer__link">
                   <a className="footer__link">
-                    {t("home_footer_companhia_contato")}
+                    {t('home_footer_companhia_contato')}
                   </a>
                 </Link>
               </li>
             </ul>
           </div>
-          
+
           <div>
             <h3 className="footer__title">
-              {t("home_footer_localizacao_titulo")}
+              {t('home_footer_localizacao_titulo')}
             </h3>
 
             <p className="footer__information">
@@ -78,17 +89,17 @@ export default function Footer() {
               Parque Capuava - Santo André/SP
             </p>
 
-            <p className="footer__information" style={{ marginTop: "10px" }}>
+            <p className="footer__information" style={{ marginTop: '10px' }}>
               CEP 09270-190
             </p>
 
-            <p className="footer__information" style={{ marginTop: "10px" }}>
+            <p className="footer__information" style={{ marginTop: '10px' }}>
               CNPJ 20.712.759/0001-04
             </p>
           </div>
 
           <div>
-            <h3 className="footer__title">{t("home_footer_redes_titulo")}</h3>
+            <h3 className="footer__title">{t('home_footer_redes_titulo')}</h3>
 
             <ul className="footer__social">
               <a
@@ -135,15 +146,14 @@ export default function Footer() {
               </a>
             </ul>
           </div>
-
         </div>
       </div>
 
       <div className="footer__info container">
-        <div  className="footer__privacy">
+        <div className="footer__privacy">
           <p
-            style={{ textAlign: "center", maxWidth: "80ch" }}
-            dangerouslySetInnerHTML={{ __html: t("home_footer_politica") }}
+            style={{ textAlign: 'center', maxWidth: '80ch' }}
+            dangerouslySetInnerHTML={{ __html: t('home_footer_politica') }}
           />
         </div>
         <span className="footer__copy">
@@ -155,5 +165,5 @@ export default function Footer() {
         </Link> */}
       </div>
     </footer>
-  );
+  )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,67 @@
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import LanguagesSwitch from "../LanguagesSwitch";
-import logoFacinnius from '../../../public/images/logoFacinnius.png'
-import Image from "next/image";
-import { useTranslation } from "react-i18next";
-import { useRouter } from "next/router";
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import LanguagesSwitch from '../LanguagesSwitch'
+import logoFacinnius from '../../../public/images/LOGOS_FACINNIUS_REGISTRADOS.png'
+import Image from 'next/image'
+import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
 
 export default function Header() {
-  const router = useRouter();
-  const [menuFixed, setMenuFixed] = useState<boolean>(false);
-  const { t } = useTranslation();
+  const router = useRouter()
+  const [menuFixed, setMenuFixed] = useState<boolean>(false)
+  const { t } = useTranslation()
 
   useEffect(() => {
-    document.addEventListener("scroll", () => {
+    document.addEventListener('scroll', () => {
       if (window.scrollY > 50) {
-        setMenuFixed(true);
+        setMenuFixed(true)
       } else if (window.scrollY === 0) {
-        setMenuFixed(false);
+        setMenuFixed(false)
       }
-    });
+    })
     return () =>
-      document.removeEventListener("scroll", () => {
-        setMenuFixed(false);
-      });
-  }, []);
+      document.removeEventListener('scroll', () => {
+        setMenuFixed(false)
+      })
+  }, [])
+
+  function logoSize() {
+    if (typeof window !== 'undefined') {
+      return window.innerWidth
+    }
+  }
 
   return (
-    <header className={`header ${menuFixed ? 'scroll-header' : ''}`} id="header">
+    <header
+      className={`header ${menuFixed ? 'scroll-header' : ''}`}
+      id="header"
+    >
       <nav className="nav container">
-        <Link href="/" className="nav__logo"  >
-          <a style={{ display: 'flex', alignItems: 'center', justifyItems: 'center' }}>
-            <Image src={logoFacinnius} alt="Logo" />
-            Facinnius
+        <Link href="/" className="nav__logo">
+          <a
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyItems: 'center'
+            }}
+          >
+            <Image
+              src={logoFacinnius}
+              alt="Logo"
+              height={logoSize() > 500 ? 42 : 30}
+              width={logoSize() > 500 ? 162 : 116}
+            />
           </a>
         </Link>
 
         <div className="nav__menu" id="nav-menu">
           <ul className="nav__list">
-            <li className={`nav__item ${router.pathname === '/' ? "active-link" : ""}`}>
-              <Link href="/" className="nav__link" >
+            <li
+              className={`nav__item ${
+                router.pathname === '/' ? 'active-link' : ''
+              }`}
+            >
+              <Link href="/" className="nav__link">
                 <a>
                   <i className="ri-home-line"></i>
                   <span>Home</span>
@@ -46,7 +69,11 @@ export default function Header() {
               </Link>
             </li>
 
-            <li className={`nav__item ${router.pathname === '/facinnius' ? "active-link" : ""}`}>
+            <li
+              className={`nav__item ${
+                router.pathname === '/facinnius' ? 'active-link' : ''
+              }`}
+            >
               <Link href="/facinnius" className="nav__link ">
                 <a>
                   <i className="ri-home-line"></i>
@@ -55,7 +82,11 @@ export default function Header() {
               </Link>
             </li>
 
-            <li className={`nav__item ${router.pathname === '/lista-produtos' ? "active-link" : ""}`} >
+            <li
+              className={`nav__item ${
+                router.pathname === '/lista-produtos' ? 'active-link' : ''
+              }`}
+            >
               <Link href="/lista-produtos" className="nav__link">
                 <a>
                   <i className="ri-price-tag-3-line"></i>
@@ -64,7 +95,11 @@ export default function Header() {
               </Link>
             </li>
 
-            <li className={`nav__item ${router.pathname === '/locais' ? "active-link" : ""}`}>
+            <li
+              className={`nav__item ${
+                router.pathname === '/locais' ? 'active-link' : ''
+              }`}
+            >
               <Link href="/locais" className="nav__link">
                 <a>
                   <i className="ri-compass-line"></i>
@@ -73,7 +108,11 @@ export default function Header() {
               </Link>
             </li>
 
-            <li className={`nav__item ${router.pathname === '/contato' ? "active-link" : ""}`}>
+            <li
+              className={`nav__item ${
+                router.pathname === '/contato' ? 'active-link' : ''
+              }`}
+            >
               <Link href="/contato" className="nav__link">
                 <a>
                   <i className="ri-t-shirt-line"></i>
@@ -84,8 +123,10 @@ export default function Header() {
           </ul>
         </div>
 
-        <i className="ri-moon-line change-theme" id="theme-button"><LanguagesSwitch /></i>
+        <i className="ri-moon-line change-theme" id="theme-button">
+          <LanguagesSwitch />
+        </i>
       </nav>
     </header>
-  );
+  )
 }
