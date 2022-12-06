38564e0c031cc35d62ffb1109f8736c7915da3eb
felipersan <feliperodrigues.san@gmail.com>
2022-12-06 16:20:39 -0300

    feat - lines component

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,25 +1,49 @@
 import React from "react";
-import Image from "next/image";
 import { useRouter } from "next/router";
-import { RiArrowRightLine } from "react-icons/ri";
 
-import Collection1 from '../../../public/images/hairdresser.png';
-import Collection2 from '../../../public/images/beautiful.png';
 import { useTranslation } from "react-i18next";
+
+import professional from "../../../public/images/professional.png";
+import homecare from "../../../public/images/homecare.png";
+import Image from "next/image";
 
 export default function Collection() {
   const { t } = useTranslation();
-  const router = useRouter()
+  const router = useRouter();
 
   function handleOnClickProduct(typo: any) {
     router.push({
       pathname: `/lista-produtos/`,
       query: { linha: typo },
-    })
+    });
   }
   return (
-    <section className="collection section" id="collection">
-      <div className="collection__container container">
+    <section className="collection container" id="collection">
+      <div className="collection__line">
+        <Image src={professional} layout="fill" objectFit="cover" />
+        <div className="collection__line__lineNameLeft">
+          <h3 className="collection__line__text">
+            {t("line_upperCase")}
+            <span className="collection__line__textBold">
+              {" "}
+              {t("profissional_upperCase")}
+            </span>
+          </h3>
+        </div>
+      </div>
+      <div className="collection__line">
+        <Image src={homecare} layout="fill" objectFit="cover" />
+        <div className="collection__line__lineNameRight">
+          <h3 className="collection__line__text">
+            {t("line_upperCase")}
+            <span className="collection__line__textBold">
+              {t("homeCare_upperCase")}
+            </span>
+          </h3>
+        </div>
+      </div>
+
+      {/* <div className="collection__container container">
         <div className="collection__explore">
           <div className="collection__data-men">
           <h2 className="collection__title" dangerouslySetInnerHTML={{ __html: t('home_colecao_masculino') }} />
@@ -41,7 +65,7 @@ export default function Collection() {
             </button>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
