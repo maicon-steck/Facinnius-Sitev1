3f0b5ccfad40a84be8c6ef00c0beb2ccccf1536a
felipersan <feliperodrigues.san@gmail.com>
2022-12-06 14:59:22 -0300

    att home page carrossel

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,132 +1,135 @@
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import LanguagesSwitch from '../LanguagesSwitch'
-import logoFacinnius from '../../../public/images/LOGOS_FACINNIUS_REGISTRADOS.png'
-import Image from 'next/image'
-import { useTranslation } from 'react-i18next'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import LanguagesSwitch from "../LanguagesSwitch";
+import logoFacinnius from "../../../public/images/LOGOS_FACINNIUS_REGISTRADOS.png";
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
 
 export default function Header() {
-  const router = useRouter()
-  const [menuFixed, setMenuFixed] = useState<boolean>(false)
-  const { t } = useTranslation()
+  const router = useRouter();
+  const [menuFixed, setMenuFixed] = useState<boolean>(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
-    document.addEventListener('scroll', () => {
+    document.addEventListener("scroll", () => {
       if (window.scrollY > 50) {
-        setMenuFixed(true)
+        setMenuFixed(true);
       } else if (window.scrollY === 0) {
-        setMenuFixed(false)
+        setMenuFixed(false);
       }
-    })
+    });
     return () =>
-      document.removeEventListener('scroll', () => {
-        setMenuFixed(false)
-      })
-  }, [])
+      document.removeEventListener("scroll", () => {
+        setMenuFixed(false);
+      });
+  }, []);
 
   function logoSize() {
-    if (typeof window !== 'undefined') {
-      return window.innerWidth
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
     }
   }
 
   return (
-    <header
-      className={`header ${menuFixed ? 'scroll-header' : ''}`}
-      id="header"
-    >
-      <nav className="nav container">
-        <Link href="/" className="nav__logo">
-          <a
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyItems: 'center'
-            }}
-          >
-            <Image
-              src={logoFacinnius}
-              alt="Logo"
-              height={logoSize() > 500 ? 42 : 30}
-              width={logoSize() > 500 ? 162 : 116}
-            />
-          </a>
-        </Link>
-
-        <div className="nav__menu" id="nav-menu">
-          <ul className="nav__list">
-            <li
-              className={`nav__item ${
-                router.pathname === '/' ? 'active-link' : ''
-              }`}
+    <>
+      <header
+        className={`header ${menuFixed ? "scroll-header" : ""}`}
+        id="header"
+      >
+        <nav className="nav container">
+          <Link href="/" className="nav__logo">
+            <a
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyItems: "center",
+              }}
             >
-              <Link href="/" className="nav__link">
-                <a>
-                  <i className="ri-home-line"></i>
-                  <span>Home</span>
-                </a>
-              </Link>
-            </li>
+              <Image
+                src={logoFacinnius}
+                alt="Logo"
+                height={logoSize() > 500 ? 42 : 30}
+                width={logoSize() > 500 ? 162 : 116}
+              />
+            </a>
+          </Link>
 
-            <li
-              className={`nav__item ${
-                router.pathname === '/facinnius' ? 'active-link' : ''
-              }`}
-            >
-              <Link href="/facinnius" className="nav__link ">
-                <a>
-                  <i className="ri-home-line"></i>
-                  <span>Facinnius</span>
-                </a>
-              </Link>
-            </li>
+          <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+              <li
+                className={`nav__item ${
+                  router.pathname === "/" ? "active-link" : ""
+                }`}
+              >
+                <Link href="/" className="nav__link">
+                  <a>
+                    <i className="ri-home-line"></i>
+                    <span>Home</span>
+                  </a>
+                </Link>
+              </li>
 
-            <li
-              className={`nav__item ${
-                router.pathname === '/lista-produtos' ? 'active-link' : ''
-              }`}
-            >
-              <Link href="/lista-produtos" className="nav__link">
-                <a>
-                  <i className="ri-price-tag-3-line"></i>
-                  <span>{t('home_heade_produtos')}</span>
-                </a>
-              </Link>
-            </li>
+              <li
+                className={`nav__item ${
+                  router.pathname === "/facinnius" ? "active-link" : ""
+                }`}
+              >
+                <Link href="/facinnius" className="nav__link ">
+                  <a>
+                    <i className="ri-home-line"></i>
+                    <span>Facinnius</span>
+                  </a>
+                </Link>
+              </li>
 
-            <li
-              className={`nav__item ${
-                router.pathname === '/locais' ? 'active-link' : ''
-              }`}
-            >
-              <Link href="/locais" className="nav__link">
-                <a>
-                  <i className="ri-compass-line"></i>
-                  <span>{t('home_header_locais')}</span>
-                </a>
-              </Link>
-            </li>
+              <li
+                className={`nav__item ${
+                  router.pathname === "/lista-produtos" ? "active-link" : ""
+                }`}
+              >
+                <Link href="/lista-produtos" className="nav__link">
+                  <a>
+                    <i className="ri-price-tag-3-line"></i>
+                    <span>{t("home_heade_produtos")}</span>
+                  </a>
+                </Link>
+              </li>
 
-            <li
-              className={`nav__item ${
-                router.pathname === '/contato' ? 'active-link' : ''
-              }`}
-            >
-              <Link href="/contato" className="nav__link">
-                <a>
-                  <i className="ri-t-shirt-line"></i>
-                  <span>{t('home_header_contato')}</span>
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+              <li
+                className={`nav__item ${
+                  router.pathname === "/locais" ? "active-link" : ""
+                }`}
+              >
+                <Link href="/locais" className="nav__link">
+                  <a>
+                    <i className="ri-compass-line"></i>
+                    <span>{t("home_header_locais")}</span>
+                  </a>
+                </Link>
+              </li>
 
-        <i className="ri-moon-line change-theme" id="theme-button">
-          <LanguagesSwitch />
-        </i>
-      </nav>
-    </header>
-  )
+              <li
+                className={`nav__item ${
+                  router.pathname === "/contato" ? "active-link" : ""
+                }`}
+              >
+                <Link href="/contato" className="nav__link">
+                  <a>
+                    <i className="ri-t-shirt-line"></i>
+                    <span>{t("home_header_contato")}</span>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <i className="ri-moon-line change-theme" id="theme-button">
+            <LanguagesSwitch />
+          </i>
+        </nav>
+        <div className="elementLine"></div>
+      </header>
+    </>
+  );
 }
