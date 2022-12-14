92cca8cee7eb10104315ece847efd25fa2b83a96
felipersan <feliperodrigues.san@gmail.com>
2022-12-13 21:49:35 -0300

    att - wppButon

--- src/components/WppBtn/index.tsx
+++ src/components/WppBtn/index.tsx
@@ -2,11 +2,7 @@ import { WppIcon } from '../../../public/icons'
 
 export default function WppBtn() {
   return (
-    <a
-      href={'https://wa.me/5511945678190'}
-      target={'_blank'}
-      className="buttonContainer"
-    >
+    <a href={'https://wa.me/5511945678190'} target={'_blank'} className="pulse">
       <WppIcon size={60} />
     </a>
   )
