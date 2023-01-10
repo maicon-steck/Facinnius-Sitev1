e61dd2a2bb0f20d8d58d9489df11665a663fae7a
felipersan <feliperodrigues.san@gmail.com>
2023-01-10 00:37:12 -0300

    update files

--- src/components/SelectDefault/index.tsx
+++ src/components/SelectDefault/index.tsx
@@ -2,51 +2,59 @@ import React, {
   useRef,
   useState,
   useCallback,
-  InputHTMLAttributes,
-} from 'react';
-import { IconBaseProps } from 'react-icons';
-import { FaAngleDown } from 'react-icons/fa';
+  InputHTMLAttributes
+} from 'react'
+import { IconBaseProps } from 'react-icons'
+import { FaAngleDown } from 'react-icons/fa'
 
 interface ErrorInput {
-  message: string;
-  isError: boolean;
+  message: string
+  isError: boolean
 }
 
 interface InputProps extends InputHTMLAttributes<HTMLSelectElement> {
-  name: string;
-  label: string;
-  error?: ErrorInput;
-  icon?: React.ComponentType<IconBaseProps>;
+  name: string
+  label: string
+  error?: ErrorInput
+  icon?: React.ComponentType<IconBaseProps>
 }
 
-export function SelectDefault({ name, label, error, children, icon: Icon, ...rest }: InputProps) {
-  const inputRef = useRef<HTMLSelectElement>(null);
+export function SelectDefault({
+  name,
+  label,
+  error,
+  children,
+  icon: Icon,
+  ...rest
+}: InputProps) {
+  const inputRef = useRef<HTMLSelectElement>(null)
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
 
   const handleInputFocus = useCallback(() => {
-    setIsFocused(true);
-  }, []);
+    setIsFocused(true)
+  }, [])
 
   const handleInputBlur = useCallback(() => {
-    setIsFocused(false);
+    setIsFocused(false)
 
-    setIsFilled(!!inputRef.current?.value);
-  }, []);
+    setIsFilled(!!inputRef.current?.value)
+  }, [])
 
   return (
-    <div className='containerSelect'>
-      <label className='labelSelect' htmlFor={name}>{label}</label>
+    <div className="containerSelect">
+      <label className="labelSelect" htmlFor={name}>
+        {label}
+      </label>
 
-      <div className='contentSelect'>
-
+      <div className="contentSelect">
         <div className="leftInputElement">
-          {Icon && <Icon color='#CCCCCC' />}
+          {Icon && <Icon color="#CCCCCC" />}
         </div>
 
         <select
-          className={`${Icon ? 'isIcon' : ''}`}
+          className={`${Icon ? 'isIcon' : ''} select`}
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
           ref={inputRef}
@@ -56,12 +64,10 @@ export function SelectDefault({ name, label, error, children, icon: Icon, ...res
           {children}
         </select>
 
-
         <div className="rightInputElement">
-          <FaAngleDown color='rgba(204, 204, 204, 1)' />
+          <FaAngleDown color="rgba(204, 204, 204, 1)" />
         </div>
-
       </div>
     </div>
-  );
+  )
 }
